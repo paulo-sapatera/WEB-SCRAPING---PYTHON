c27b6c16dd98336a67fb1c9ed78ed61410e279ce
Paulo Sapatera <sapateraneto@hotmail.com>
2021-12-29 22:39:11 -0300

    Web Scraping

---
+++ scraper.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.netshoes.com.br/chuteiras/masculino?mi=hm_ger_mntop_H-CAL-calcados-chuteiras&psn=Menu_Top'
+
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
+
+site = requests.get(url, headers=headers)
+soup = BeautifulSoup(site.content, "html.parser")
+chuteiras = soup.find_all 
