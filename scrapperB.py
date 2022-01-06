3c23df527058cfe81f1f43a3aef17e2e81a6edfc
Paulo Sapatera <sapateraneto@hotmail.com>
2022-01-06 11:36:26 -0300

    provisorio

--- scrapperB.py
+++ scrapperB.py
@@ -1,17 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.brasiltronic.com.br/foto-video/cameras-digitais'
+url = 'https://www.balaodainformatica.com.br/perifericos/teclados'
 
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
 
 site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
-cameras = soup.find_all('div', class_='inner')
+teclados = soup.find_all('div', class_='.products-grid .main-info')
 ultima_pagina = soup.find('span', class_='page-selected last')
 
-camera = cameras[0]
-marca = camera.find('a', class_= 'link-all')
+teclado = teclados[0]
+marca = teclado.find('a', class_= 'link-all')
 
 
 print(marca)
